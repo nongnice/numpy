c6715953afed644992817a8e5da291d8dd48a969
Nice <niceseti2007@gmail.com>
2025-07-05 12:37:56 +0700

    Create matrix

---
+++ manyfuction.py
@@ -0,0 +1,18 @@
+import numpy as np
+a=np.zeros((2,2))
+print(a)
+
+b=np.ones((1,2))
+print(b)
+
+c=np.full((2,2),7)
+print(c)
+
+d=np.eye(2)
+print(d)
+
+e=np.eye(3)
+print(e)
+
+f=np.random.random((2,2))
+print(f)
